aee89a0bf0cdf74393f32b09486a3df3f1021c1c
Eduardo R <EDD-3@users.noreply.github.com>
2020-05-19 14:16:56 -0700

    Added virtual environment python

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, redirect, flash, url_for,jsonify
+from flask import Flask, render_template, request, jsonify
 from ssl import SSLContext
-from werkzeug import secure_filename
 import json
 from urllib.request import urlopen
 
@@ -9,25 +8,27 @@ from urllib.request import urlopen
 app     = Flask(__name__)
 app.config.from_object("config")
 
+if __name__ == "__main__":
+    app.run()
+    app.debug = True
 
 from helpers import *
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
-    app.debug = True
+
 
 @app.route("/images", methods=["POST"])
 def upload_file():
-    file_info = json.loads(request.data)
-    if file_info and file_info['urlImage']:
+    try:
+        file_info = json.loads(request.data)
         response = urlopen(file_info['urlImage'], context=SSLContext())
+
         if response.code == 200:
             output = upload_file_to_s3(response , app.config["S3_BUCKET"])
-            return jsonify({"imageUrl": output}), 200
-    else:
-        return redirect("/")
+            return jsonify(output), 200
+
+    except Exception as e:
+        return jsonify({'Exception': e}),500
